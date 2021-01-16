b7341a865929477b58f42cd061281b82f0bfa67a
Ghazanfar <ghazanfarmalik345@gmail.com>
2021-01-16 21:51:59 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react'
+import Stopwatch from './stopwatch.jsx';
+// import stopwatch from './stopwatch.jsx'
+
+class App extends React.Component{
+  render(){
+    return <div>
+      <Stopwatch />
+      </div>
+  }
+}
+
+export default App;
